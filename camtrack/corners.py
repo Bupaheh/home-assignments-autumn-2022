7515ea4bb9344118f94426c24357af394ef8f95f
Pavel Balay <pavel.balai@gmail.com>
2022-09-29 01:15:32 +0300

    Implement first version

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -27,7 +27,7 @@ from _corners import (
     calc_track_interval_mappings,
     calc_track_len_array_mapping,
     without_short_tracks,
-    create_cli
+    create_cli, filter_frame_corners
 )
 
 
@@ -48,17 +48,50 @@ class _CornerStorageBuilder:
 
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
-    # TODO
+    corner_params = dict(maxCorners=0, qualityLevel=0.01, minDistance=3)
+    lk_params = dict(winSize=(20, 20), maxLevel=3)
+    barrier = 10.0
+
     image_0 = frame_sequence[0]
-    corners = FrameCorners(
-        np.array([0]),
-        np.array([[0, 0]]),
-        np.array([55])
+    corners = cv2.goodFeaturesToTrack(image_0, **corner_params)
+    prev_corner_frames = FrameCorners(
+        np.arange(corners.shape[0]),
+        corners,
+        np.ones(corners.shape[0]) * 30
     )
-    builder.set_corners_at_frame(0, corners)
+    builder.set_corners_at_frame(0, prev_corner_frames)
+
+    id_cnt = corners.shape[0]
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
-        builder.set_corners_at_frame(frame, corners)
+        next_pts, statuses, _ = cv2.calcOpticalFlowPyrLK(np.ubyte(image_0 * 255), np.ubyte(image_1 * 255),
+                                                         prev_corner_frames.points, None, **lk_params)
+
+        corners_1 = cv2.goodFeaturesToTrack(image_1, **corner_params)
+
+        ids = np.ones(corners_1.shape[0], int) * -1
+        for pt, corner_id, status in zip(next_pts, prev_corner_frames.ids, statuses):
+            if status == 0:
+                continue
+
+            distances = np.sqrt((corners_1[:, 0, 0] - pt[0]) ** 2 + (corners_1[:, 0, 1] - pt[1]) ** 2)
+            index = distances.argmin()
+            min_distance = distances.min()
+            if min_distance < barrier:
+                ids[index] = corner_id
+
+        for i in range(len(ids)):
+            if ids[i] == -1:
+                ids[i] = id_cnt
+                id_cnt += 1
+
+        corner_frames = FrameCorners(
+            ids,
+            corners_1,
+            np.ones(corners_1.shape[0]) * 30
+        )
         image_0 = image_1
+        builder.set_corners_at_frame(frame, corner_frames)
+        prev_corner_frames = corner_frames
 
 
 def build(frame_sequence: pims.FramesSequence,
